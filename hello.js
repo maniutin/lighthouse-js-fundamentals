642e989a5fd849f90ed9b7dfc61060c5c2f92181
Nick Maniutin <maniutin@gmail.com>
2020-12-29 02:09:16 +0000

    Using return vs. console.log

--- hello.js
+++ hello.js
@@ -6,3 +6,9 @@ sayHello("dude");
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+};
+const greeting = returnSayHello("John");
+console.log(greeting);
